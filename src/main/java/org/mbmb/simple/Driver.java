ecc259a1cadfee28bb0d8d0b3318dcde0f7849df
Marcus Borch <mborch@idtus.com>
2018-05-16 15:05:45 -0400

    publishsubject

--- src/main/java/org/mbmb/simple/Driver.java
+++ src/main/java/org/mbmb/simple/Driver.java
@@ -1,14 +1,58 @@
 package org.mbmb.simple;
 
-import java.util.concurrent.atomic.AtomicReference;
-import rx.Observable;
+import rx.Observer;
+import rx.subjects.PublishSubject;
 
 public class Driver {
+	static Integer subscriber1 = 0;
+	static Integer subscriber2 = 0;
 
-	public static void main(String[] args) {
-		AtomicReference<String> result = new AtomicReference<>("");
-		Observable<String> observer = Observable.just("Hello");
-		observer.subscribe(result::set);
-		System.out.println(result);
+	static Observer<Integer> getFirstObserver() {
+		return new Observer<Integer>() {
+			@Override
+			public void onNext(Integer value) {
+				subscriber1 += value;
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				System.out.println("error");
+			}
+
+			@Override
+			public void onCompleted() {
+				System.out.println("Subscriber1 completed: " + subscriber1);
+			}
+		};
+	}
+
+	static Observer<Integer> getSecondObserver() {
+		return new Observer<Integer>() {
+			@Override
+			public void onNext(Integer value) {
+				subscriber2 += value;
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				System.out.println("error");
+			}
+
+			@Override
+			public void onCompleted() {
+				System.out.println("Subscriber2 completed: " + subscriber2);
+			}
+		};
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		PublishSubject<Integer> subject = PublishSubject.create();
+		subject.subscribe(getFirstObserver());
+		subject.onNext(1);
+		subject.onNext(2);
+		subject.onNext(3);
+		subject.subscribe(getSecondObserver());
+		subject.onNext(4);
+		subject.onCompleted();
 	}
 }
