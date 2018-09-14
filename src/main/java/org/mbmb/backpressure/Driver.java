cfb7a0436ce5c2125f5d272746016b949c8746b2
Marcus Borch <mborch@idtus.com>
2018-09-14 10:46:21 -0400

    hot and cold

--- src/main/java/org/mbmb/backpressure/Driver.java
+++ src/main/java/org/mbmb/backpressure/Driver.java
@@ -1,20 +1,35 @@
 package org.mbmb.backpressure;
 
 import java.util.stream.IntStream;
+import rx.Observable;
 import rx.schedulers.Schedulers;
 import rx.subjects.PublishSubject;
 
 public class Driver {
 
-	public static void main(String[] args) {
-//		Observable.range(1, 10)
-//				.observeOn(Schedulers.computation())
-//				.subscribe(ComputeFunction::compute);
-		PublishSubject<Integer> source = PublishSubject.<Integer>create();
+    public static void coldObservable() throws InterruptedException {
+        Observable.range(1, 10)
+            .observeOn(Schedulers.computation())
+            .subscribe(ComputeFunction::compute);
+        Thread.sleep(11000);
+    }
 
-		source.observeOn(Schedulers.computation())
-				.subscribe(ComputeFunction::compute, Throwable::printStackTrace);
+    public static void hotObservable() throws InterruptedException {
+        PublishSubject<Integer> source = PublishSubject.create();
 
-		IntStream.range(1, 1_000_000).forEach(source::onNext);
-	}
+        source.observeOn(Schedulers.computation())
+            .subscribe(ComputeFunction::compute, Throwable::printStackTrace);
+
+        IntStream.range(1, 10).forEach(i -> {
+            source.onNext(i);
+            System.out.println(i);
+        });
+
+        Thread.sleep(11000);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        coldObservable();
+//        hotObservable();
+    }
 }
