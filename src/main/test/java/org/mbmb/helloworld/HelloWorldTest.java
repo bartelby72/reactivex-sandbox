345cd3dbed69f8abc346908b213d33a3fc0e318f
Marcus Borch <mborch@idtus.com>
2018-09-14 05:34:06 -0400

    helloworld test

--- src/main/test/java/org/mbmb/helloworld/HelloWorldTest.java
+++ src/main/test/java/org/mbmb/helloworld/HelloWorldTest.java
@@ -1,5 +1,7 @@
 package org.mbmb.helloworld;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import rx.Observable;
 
@@ -7,13 +9,35 @@ import rx.Observable;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HelloWorldTest {
+    private String result;
+
+    @BeforeEach
+    public void setup() {
+        this.result = "";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.result = null;
+    }
 
     @Test
     public void hello() {
-        final String[] result = {""};
         Observable<String> observable = Observable.just("Hello");
-        observable.subscribe(s -> result[0] = s);
+        observable.subscribe(s -> result = s);
 
-        assertTrue(result[0].equals("Hello"));
+        assertTrue(result.equals("Hello"));
+    }
+
+    @Test
+    public void methodBreakout() {
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g"};
+        Observable<String> observable = Observable.from(letters);
+        observable.subscribe(
+            i -> result += i,  //OnNext
+            Throwable::printStackTrace, //OnError
+            () -> result += "_Completed" //OnCompleted
+        );
+        assertTrue(result.equals("abcdefg_Completed"));
     }
 }
