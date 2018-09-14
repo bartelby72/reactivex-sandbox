51340a05436528d78407072f64a1a6f826dfc246
Marcus Borch <mborch@idtus.com>
2018-09-14 10:03:45 -0400

    resource using

--- src/main/test/java/org/mbmb/helloworld/OtherObservableTest.java
+++ src/main/test/java/org/mbmb/helloworld/OtherObservableTest.java
@@ -81,4 +81,24 @@ public class OtherObservableTest {
 
         assertTrue(subscriber1 + subscriber2 == 14);
     }
+
+    @Test
+    public void resourceUsing() {
+        String[] result = {""};
+        Observable<Character> values = Observable.using(
+            () -> "MyResource",
+            r -> Observable.create(o -> {
+                for (Character c : r.toCharArray()) {
+                    o.onNext(c);
+                }
+                o.onCompleted();
+            }),
+            r -> System.out.println("Disposed: " + r)
+        );
+        values.subscribe(
+            v -> result[0] += v,
+            e -> result[0] += e
+        );
+        assertTrue(result[0].equals("MyResource"));
+    }
 }
