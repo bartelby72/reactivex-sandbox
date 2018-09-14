704e0b6663795742d248cced44fa4c3d0d835746
Marcus Borch <mborch@idtus.com>
2018-09-14 09:53:18 -0400

    subjects are pretty dope

---
+++ src/main/test/java/org/mbmb/helloworld/OtherObservableTest.java
@@ -0,0 +1,84 @@
+package org.mbmb.helloworld;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+import rx.Observable;
+import rx.Observer;
+import rx.observables.ConnectableObservable;
+import rx.subjects.PublishSubject;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OtherObservableTest {
+
+    @Test
+    public void connectableObservable() throws InterruptedException {
+        final String[] result = {""};
+        ConnectableObservable<Long> connectable
+            = Observable.interval(200, TimeUnit.MILLISECONDS).publish();
+        connectable.subscribe(i -> result[0] += i);
+        assertFalse(result[0].equals("01"));
+
+        connectable.connect();
+        Thread.sleep(500);
+
+        assertTrue(result[0].equals("01"));
+    }
+
+    Integer subscriber1 = 0;
+    Integer subscriber2 = 0;
+
+    Observer<Integer> getFirstObserver() {
+        return new Observer<Integer>() {
+            @Override
+            public void onNext(Integer value) {
+                subscriber1 += value;
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("error");
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Subscriber1 completed");
+            }
+        };
+    }
+
+    Observer<Integer> getSecondObserver() {
+        return new Observer<Integer>() {
+            @Override
+            public void onNext(Integer value) {
+                subscriber2 += value;
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("error");
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Subscriber2 completed");
+            }
+        };
+    }
+
+    @Test
+    public void subscriber() {
+        PublishSubject<Integer> subject = PublishSubject.create();
+        subject.subscribe(getFirstObserver());
+        subject.onNext(1);
+        subject.onNext(2);
+        subject.onNext(3);
+        subject.subscribe(getSecondObserver());
+        subject.onNext(4);
+        subject.onCompleted();
+
+        assertTrue(subscriber1 + subscriber2 == 14);
+    }
+}
