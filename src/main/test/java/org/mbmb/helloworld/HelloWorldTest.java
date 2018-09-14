3caacc95a3a70ad8ba7b7ce899a78c1684349bf9
Marcus Borch <mborch@idtus.com>
2018-09-14 08:59:31 -0400

    flatmap

--- src/main/test/java/org/mbmb/helloworld/HelloWorldTest.java
+++ src/main/test/java/org/mbmb/helloworld/HelloWorldTest.java
@@ -1,13 +1,22 @@
 package org.mbmb.helloworld;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import rx.Observable;
+import rx.schedulers.TestScheduler;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * from https://www.baeldung.com/rx-java
+ */
 public class HelloWorldTest {
     private String result;
 
@@ -39,5 +48,40 @@ public class HelloWorldTest {
             () -> result += "_Completed" //OnCompleted
         );
         assertTrue(result.equals("abcdefg_Completed"));
+    }
+
+    @Test
+    public void map() {
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g"};
+        Observable.from(letters)
+            .map(String::toUpperCase)
+            .subscribe(letter -> result += letter);
+        assertTrue(result.equals("ABCDEFG"));
+    }
+
+    /**
+     * https://medium.com/appunite-edu-collection/rxjava-flatmap-switchmap-and-concatmap-differences-examples-6d1f3ff88ee0
+     */
+    @Test
+    public void flatmap() {
+        final List<String> items = Arrays.asList("a", "b", "c", "d", "e", "f");
+
+        final TestScheduler scheduler = new TestScheduler();
+
+        Observable.from(items)
+            .flatMap(s -> {
+                final int delay = new Random().nextInt(10);
+                return Observable.just(s + "x")
+                    .delay(delay, TimeUnit.SECONDS, scheduler);
+            })
+            .toList()
+            .doOnNext(l -> {
+                l.sort(String::compareTo);
+                l.forEach(s -> result += s);
+            })
+            .subscribe();
+
+        scheduler.advanceTimeBy(1, TimeUnit.MINUTES);
+        assertEquals("axbxcxdxexfx", result);
     }
 }
