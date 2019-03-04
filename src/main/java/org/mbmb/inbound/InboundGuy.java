e0fa92bbb0cc45e0f99a7a1199fd4e9465a6d32d
Marcus Borch <mborch@idtus.com>
2019-03-04 05:17:36 -0500

    enhanced output

--- src/main/java/org/mbmb/inbound/InboundGuy.java
+++ src/main/java/org/mbmb/inbound/InboundGuy.java
@@ -1,76 +1,33 @@
 package org.mbmb.inbound;
 
 import io.reactivex.Observable;
-import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("ALL")
 public class InboundGuy {
-
-    private void testWithCountdownLatch() throws InterruptedException {
-        CountDownLatch done = new CountDownLatch(2);
-        final List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
-
-        Observable<Integer> observeMe = Observable.<Integer>create(emitter -> {
-                integers.forEach(emitter::onNext);
-                emitter.onComplete();
-            }
-        )
-//            .observeOn(Schedulers.newThread())
-//            .subscribeOn(Schedulers.newThread())
-            .doOnError(Throwable::printStackTrace)
-//            .share()
-            ;
-//        Observable<Integer> observeMe = Observable.just(1, 2, 3, 4, 5)
-////            .observeOn(Schedulers.io())
-//            .doOnError(Throwable::printStackTrace)
-//            .share();
-        CompositeDisposable compositeDisposable = new CompositeDisposable();
-        compositeDisposable.add(observeMe
-//                .observeOn(Schedulers.newThread())
-                .doOnNext(integer -> {
-                    final String threadName = "d0.thread";
-                    Thread.currentThread().setName(threadName);
-                    System.out.println(Thread.currentThread().getName() + ": " + integer);
-                    System.out.flush();
-                })
-                .doOnComplete(done::countDown)
-                .subscribeOn(Schedulers.newThread())
-                .subscribe()
-        );
-        compositeDisposable.add(observeMe
-//                .observeOn(Schedulers.newThread())
-                .doOnNext(integer -> {
-                    final String threadName = "d1.thread";
-                    Thread.currentThread().setName(threadName);
-                    System.out.println(Thread.currentThread().getName() + ": " + integer);
-                    System.out.flush();
-                })
-                .doOnComplete(done::countDown)
-                .subscribeOn(Schedulers.newThread())
-                .subscribe()
-        );
-        done.await(3, TimeUnit.SECONDS);
-    }
+    private final static int OBS_COUNT = 5;
 
     private void testObserveOn() {
-        Observable.create(emitter -> {
+        Observable<Integer> observable = Observable.<Integer>create(emitter -> {
             Arrays.asList(1, 2, 3, 4, 5)
                 .forEach(i -> {
                     System.err.println(Thread.currentThread().getName() + ": testObserveOn: emitting: " + i);
                     emitter.onNext(i);
                 });
         })
-            .observeOn(Schedulers.newThread())
-            .doOnNext(o -> {
-                System.err.println(Thread.currentThread().getName() + ": testObserveOn: onNext: " + o);
-            })
-            .subscribe();
+            .observeOn(Schedulers.newThread());
+        //
+        for (int lcv = 0; lcv < OBS_COUNT; lcv++) {
+            int flcv = lcv;
+            observable
+                .doOnNext(o -> {
+                    System.err.println(Thread.currentThread().getName() + "[" + flcv + "]: testObserveOn: onNext: " + o);
+                })
+                .subscribe();
+        }
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -81,11 +38,17 @@ public class InboundGuy {
     private void testSubjectObserveOn() {
         PublishSubject<Integer> publishSubject = PublishSubject.create();
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
-        publishSubject.observeOn(Schedulers.newThread())
-            .doOnNext(o -> {
-                System.err.println(Thread.currentThread().getName() + ": testSubjectObserveOn: onNext: " + o);
-            })
-            .subscribe();
+        Observable<Integer> observable = publishSubject.observeOn(Schedulers.newThread());
+        //
+        for (int lcv = 0; lcv < OBS_COUNT; lcv++) {
+            int flcv = lcv;
+            observable
+                .doOnNext(o -> {
+                    System.err.println(Thread.currentThread().getName() + "[" + flcv + "]: testSubjectObserveOn: onNext: " + o);
+                })
+                .subscribe();
+        }
+        //
         integers.forEach(i -> {
             System.err.println(Thread.currentThread().getName() + ": testSubjectObserveOn: emitting: " + i);
             publishSubject.onNext(i);
@@ -98,18 +61,23 @@ public class InboundGuy {
     }
 
     private void testSubscribeOn() {
-        Observable.create(emitter -> {
+        Observable<Integer> observable = Observable.<Integer>create(emitter -> {
             Arrays.asList(1, 2, 3, 4, 5)
                 .forEach(i -> {
                     System.err.println(Thread.currentThread().getName() + ": testSubscribeOn: emitting: " + i);
                     emitter.onNext(i);
                 });
         })
-            .subscribeOn(Schedulers.newThread())
-            .doOnNext(o -> {
-                System.err.println(Thread.currentThread().getName() + ": testSubscribeOn: onNext: " + o);
-            })
-            .subscribe();
+            .subscribeOn(Schedulers.newThread());
+        //
+        for (int lcv = 0; lcv < OBS_COUNT; lcv++) {
+            int flcv = lcv;
+            observable
+                .doOnNext(o -> {
+                    System.err.println(Thread.currentThread().getName() + "[" + flcv + "]: testSubscribeOn: onNext: " + o);
+                })
+                .subscribe();
+        }
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -120,11 +88,17 @@ public class InboundGuy {
     private void testSubjectSubscribeOn() {
         PublishSubject<Integer> publishSubject = PublishSubject.create();
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
-        publishSubject.subscribeOn(Schedulers.newThread())
-            .doOnNext(o -> {
-                System.err.println(Thread.currentThread().getName() + ": testSubjectSubscribeOn: onNext: " + o);
-            })
-            .subscribe();
+        Observable<Integer> observable = publishSubject.subscribeOn(Schedulers.newThread());
+        //
+        for (int lcv = 0; lcv < OBS_COUNT; lcv++) {
+            int flcv = lcv;
+            observable
+                .doOnNext(o -> {
+                    System.err.println(Thread.currentThread().getName() + "[" + flcv + "]: testSubjectObserveOn: onNext: " + o);
+                })
+                .subscribe();
+        }
+        //
         integers.forEach(i -> {
             System.err.println(Thread.currentThread().getName() + ": testSubjectSubscribeOn: emitting: " + i);
             publishSubject.onNext(i);
@@ -139,10 +113,12 @@ public class InboundGuy {
     public static void main(String[] args) throws InterruptedException {
         InboundGuy inboundGuy = new InboundGuy();
 
-//        inboundGuy.testWithCountdownLatch();
         inboundGuy.testObserveOn();
+        System.err.println("/////////////////////////////////////////////////");
         inboundGuy.testSubjectObserveOn();
+        System.err.println("/////////////////////////////////////////////////");
         inboundGuy.testSubscribeOn();
+        System.err.println("/////////////////////////////////////////////////");
         inboundGuy.testSubjectSubscribeOn();
     }
 }
